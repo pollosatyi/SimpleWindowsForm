ddb65a33a8c7733c9790d996aea6e96513a3def2
pollosatyi <pollosatyi@mail.ru>
2024-04-21 12:32:07 +0300

    1

--- SimpleForm/Form1.Designer.cs
+++ SimpleForm/Form1.Designer.cs
@@ -29,19 +29,88 @@
         private void InitializeComponent()
         {
             splitContainer1 = new SplitContainer();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            nudResolution = new NumericUpDown();
+            nudDensity = new NumericUpDown();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
             splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudResolution).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudDensity).BeginInit();
             SuspendLayout();
             // 
             // splitContainer1
             // 
             splitContainer1.BorderStyle = BorderStyle.Fixed3D;
             splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.FixedPanel = FixedPanel.Panel1;
+            splitContainer1.IsSplitterFixed = true;
             splitContainer1.Location = new Point(0, 0);
             splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(nudDensity);
+            splitContainer1.Panel1.Controls.Add(label2);
+            splitContainer1.Panel1.Controls.Add(nudResolution);
+            splitContainer1.Panel1.Controls.Add(label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(pictureBox1);
             splitContainer1.Size = new Size(800, 450);
-            splitContainer1.SplitterDistance = 266;
+            splitContainer1.SplitterDistance = 138;
             splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(654, 446);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(9, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Resolution";
+            // 
+            // nudResolution
+            // 
+            nudResolution.Location = new Point(9, 41);
+            nudResolution.Name = "nudResolution";
+            nudResolution.Size = new Size(120, 23);
+            nudResolution.TabIndex = 2;
+            nudResolution.TextAlign = HorizontalAlignment.Right;
+            // 
+            // nudDensity
+            // 
+            nudDensity.Location = new Point(10, 108);
+            nudDensity.Name = "nudDensity";
+            nudDensity.Size = new Size(120, 23);
+            nudDensity.TabIndex = 4;
+            nudDensity.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(10, 80);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Density";
+            label2.Click += label2_Click;
             // 
             // Form1
             // 
@@ -51,13 +120,24 @@
             Controls.Add(splitContainer1);
             Name = "Form1";
             Text = "Form1";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
             splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudResolution).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudDensity).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private SplitContainer splitContainer1;
+        private PictureBox pictureBox1;
+        private NumericUpDown nudResolution;
+        private Label label1;
+        private NumericUpDown nudDensity;
+        private Label label2;
     }
 }
--- SimpleForm/Form1.cs
+++ SimpleForm/Form1.cs
@@ -6,5 +6,10 @@ namespace SimpleForm
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
